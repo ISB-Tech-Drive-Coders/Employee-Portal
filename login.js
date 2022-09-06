5bceb2eb073c3600e5846c0769285f3bf762ee92
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2022-09-06 11:41:47 -0700

    Just make a next app :|

--- login.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-
-const SUPABASE_URL = "https://fndnkiseothtahipisqa.supabase.co"
-
-const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_KEY);
--- readme.md
+++
@@ -1,1 +0,0 @@
-# ISBTD Employee Portal
